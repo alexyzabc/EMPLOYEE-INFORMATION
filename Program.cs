b9d238fe9ffafcc485bb4fbdd7362f6db4904363
Claveria <claveriaency@gmail.com>
2026-02-21 17:49:32 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Data;
+
+namespace EMPLOYEE_INFORMATION
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==EMPLOYEE INFORMATION (PROMOTION, MOVEMENT, AND HIRING==");
+            Console.WriteLine("1. Add Employee(CREATE)");
+            Console.WriteLine("2. View Employee(RETRIEVE)");
+            Console.WriteLine("3. Update Employee(UPDATE)");
+            Console.WriteLine("4. Delete Employee(DELETE)");
+
+            string fname, lname;
+            char mInitial;
+            int age, employeeId;
+            Console.Write("Enter your First Name: ");
+            Console.Write("Enter your Last Name: ");
+            Console.Write("Enter your age: ");
+            Console.Write("Enter your Employee ID: ");
+
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    AddEmployee();
+                    break;
+                case 2:
+                    ViewEmployee();
+                    break;
+                case 3:
+                    UpdateEmployee();
+                    break;
+                case 4:
+                    DeleteEmployee();
+                    break;
+         if (employ ==  )
+
+            }
+        }
+    }
+}
