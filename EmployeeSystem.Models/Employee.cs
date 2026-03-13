50dc41cfeec7fc54e9578233803331b90e2879f4
alexyzabc <aamariereyes@gmail.com>
2026-03-13 09:57:47 +0800

    sure na to huhuhu ata

---
+++ EmployeeSystem.Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeSystem.Models
+{
+    public class Employee
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public string Department { get; set; }
+        public string Status { get; set; }
+    }
+}
