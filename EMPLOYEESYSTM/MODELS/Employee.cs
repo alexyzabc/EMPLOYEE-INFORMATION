c4465ae53d4e98892d20a92f69601014abf9a78d
alexyzabc <aamariereyes@gmail.com>
2026-04-06 04:32:25 +0800

    Re-add main project files

---
+++ EMPLOYEESYSTM/MODELS/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EmployeeSystem.Models
+{
+    public class Employee
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public string Department { get; set; }
+        public string Status { get; set; }
+    }
+}
+
+//models
